eb3f2558e5ca282efcb9fa0baccfbfa8bc2995d6
lbkchen <lbkchen@gmail.com>
2016-05-26 15:27:12 -0700

    MRN number part of js done, may be buggy, working on querying request logs

--- server/database.js
+++ server/database.js
@@ -25,14 +25,53 @@ app.use(multer());
 
 app.post('/', function(req, res) {
     var request = req.body;
+
+
+    // Checking FIN patient MRN number
     if (request["Target Action"] == "check FIN") {
+        var connection = db.createConnection(connectionInfo);
+        connection.connect(function(err) {
+            if (err) {
+                console.error("Error connecting: " + err.stack);
+                return;
+            }
+            console.log('Connected as id ' + connection.threadId);
+        });
+
+        var fin = request["FIN"];
+        var table = request["Table"];
+        var query = "USE cvicu; SELECT * from ? where FIN = ?";
+        console.log(sql);
+        connection.query(query, [table, fin], function(err, results) {
+            if (err) {
+                console.error("Error in query: " + err.stack);
+                return;
+            } else if (isEmpty(results)) {
+                // No such user exists
+                console.log("FIN and complication not found!");
+                res.write("NO");
+                res.end();
+            } else {
+                // User found
+                console.log("FIN and complication found!");
+                res.write(results[results.length - 1]["date"]);
+                res.end();
+            }
+        });
+        connection.end();
+    }
+
+
+    // All queries requesting logs
+    if (request["Target Action"] == "requestLogs") {
+        var backMessage = [];
         var connection = db.createConnection(connectionInfo);
         connection.connect();
 
-        var query = "USE cvicu; SELECT * from "
-        connection.query("USE cvicu");
-
+        connection.query() //TODO: Figure out why logs are nested within each other (mistake?). Also, how do we get the particular complication from the connection before query? Use multiple dictionaries to make modular code?
     }
+
+    // All queries inserting into logs
 });
 
 app.listen(3000, function() {
