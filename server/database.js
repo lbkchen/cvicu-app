26f81de212d509e80ff5472d3b887dea5622c0db
lbkchen <lbkchen@gmail.com>
2016-05-27 11:27:05 -0700

    finished insert into set part of database.js code

--- server/database.js
+++ server/database.js
@@ -12,7 +12,18 @@ var connectionInfo = {
     user : "root",
     password : "mysql",
     port : 3306
-}
+};
+
+// Complications maps
+
+var arrhythmia = {
+    tableName : "arrhythmialog",
+    columnNames : ["FIN", "Type", "Therapy", "StopDate", "date", "date_1"]
+};
+
+var complicationTables = {
+    arrhythmialog : arrythmia
+};
 
 // Configure app
 app.use(bodyParser.json());
@@ -40,11 +51,11 @@ app.post('/', function(req, res) {
 
         var fin = request["FIN"];
         var table = request["Table"];
-        var query = "USE cvicu; SELECT * from ? where FIN = ?";
+        var query = "USE cvicu; SELECT * from ? where FIN = ?;";
         console.log(sql);
         connection.query(query, [table, fin], function(err, results) {
             if (err) {
-                console.error("Error in query: " + err.stack);
+                console.error("Error in FIN query: " + err.stack);
                 return;
             } else if (isEmpty(results)) {
                 // No such user exists
@@ -68,10 +79,50 @@ app.post('/', function(req, res) {
         var connection = db.createConnection(connectionInfo);
         connection.connect();
 
-        connection.query() //TODO: Figure out why logs are nested within each other (mistake?). Also, how do we get the particular complication from the connection before query? Use multiple dictionaries to make modular code?
+        connection.query(); //TODO: Figure out why logs are nested within each other (mistake?). Also, how do we get the particular complication from the connection before query? Use multiple dictionaries to make modular code?
     }
 
     // All queries inserting into logs
+    if (request["Target Action"] == "add log") {
+        // Create connection and get table name
+        var connection = db.createConnection(connectionInfo);
+        var thisComplication = request["Table"];
+
+        console.log ("Start: adding to " + thisComplication);
+
+        // Create an object patientLog that represents a row of values in the SQL table
+        var patientLog = {};
+        var columnNames = complicationTables[thisComplication]["columnNames"];
+        for (column in columnNames) {
+            patientLog[column] = request[column];
+        }
+
+        // Generate query
+        var query = "USE cvicu; INSERT INTO ? SET ?;";
+        connection.query(query, [thisComplication, patientLog], function(err, results) {
+            if (err) {
+                console.error("Error in " + thisComplication + " insertion: " + err.stack);
+                var toClient = {
+                    BackMsg : "add back",
+                    Result : "false"
+                };
+                toClient = JSON.stringify(toClient);
+                res.write(toClient);
+                res.end();
+                return;
+            } else {
+                var toClient = {
+                    BackMsg : "add back",
+                    Result : "true"
+                };
+                toClient = JSON.stringify(toClient);
+                res.write(toClient);
+                res.end();
+            }
+        });
+        connection.end();
+        console.log("End: adding to " + thisComplication);
+    }
 });
 
 app.listen(3000, function() {
