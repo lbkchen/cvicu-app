6fb88e6d0d1c0d631a36bbe046144685850edb45
lbkchen <lbkchen@gmail.com>
2016-06-01 17:01:17 -0700

    collectionview is not being recognized! consider making a collectionviewcontroller and programmatically adding it via the outer controller

--- complicationsapp/complicationsapp/ComplicationHistoryCollectionView.swift
+++ complicationsapp/complicationsapp/ComplicationHistoryCollectionView.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
-class ComplicationHistoryCollectionView : UICollectionView {
+class ComplicationHistoryCollectionView : UICollectionView, UICollectionViewDataSource, UICollectionViewDelegate {
+    var complications = ["Cardiopulmonary resuscitation",
+                         "Unplanned return to CICU (<48 hours)",
+                         "Unplanned readmission to the hospital within 30 days",
+                         "Arrhythmia",
+                         "Mechanical circulatory support during CICU encounter",
+                         "Low Cardiac Output Syndrome",
+                         "Pericardial effusion requiring drainage",
+                         "Pulmonary hypertension",
+                         "Pulmonary vein obstruction",
+                         "Systemic vein obstruction",
+                         "RESPIRATORY",
+                         "Listed for heart transplant during CICU encounter",
+                         "Reoperation for bleeding",
+                         "ORGAN DYSFUNCTION",
+                         "Delayed Sternal Closure",
+                         "Intraoperative death or intraprocedural death",
+                         "Infections",
+                         "Unplanned operation/procedure"].sort()
+    
+    @IBOutlet weak var historyLabel: UILabel!
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return complications.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = dequeueReusableCellWithReuseIdentifier("history", forIndexPath: indexPath) as! ComplicationHistoryCollectionViewCell
+        
+        cell.historyLabel.text = complications[indexPath.row]
+        
+        return cell
+    }
     
 }
---
+++ complicationsapp/complicationsapp/ComplicationHistoryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ComplicationHistoryCollectionViewCell.swift
+//  complicationsapp
+//
+//  Created by Ken Chen on 6/1/16.
+//  Copyright © 2016 cvicu. All rights reserved.
+//
+
+import UIKit
+
+class ComplicationHistoryCollectionViewCell : UICollectionViewCell {
+    @IBOutlet weak var historyLabel: UILabel!
+    
+}
